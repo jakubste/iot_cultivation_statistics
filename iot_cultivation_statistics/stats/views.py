defdb7fedd9998763e80de07867c6e4c42adce85
Jakub Stępak <jakub.ste@gmail.com>
2016-04-17 19:22:53 +0200

    Model improvements, basic view

--- iot_cultivation_statistics/stats/views.py
+++ iot_cultivation_statistics/stats/views.py
@@ -1,9 +1,5 @@
-from django.shortcuts import render
-from django.shortcuts import redirect
-from django.views.decorators.debug import sensitive_variables
-from django.views.generic import TemplateView
-from django.views.generic.edit import FormView
+from django.views.generic.list import ListView
 
 
-class Stats(TemplateView):
-    template_name = 'stats.html'
+class PlantList(ListView):
+    template_name = 'plant_list.html'
